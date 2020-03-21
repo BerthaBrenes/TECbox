d16cb113d41e72beab014b1e8c8715800949e307
Bertha Brenes <bertacaro1996@hotmail.com>
2020-03-20 21:03:23 -0600

    [ADD]style webAPI

--- webPage/src/app/pages/articulos/articulos.component.ts
+++ webPage/src/app/pages/articulos/articulos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./articulos.component.scss'],
 })
 export class ArticulosComponent implements OnInit {
-  data:any;
+  data:any=[{"name":"MemoriaSD","description":"Memoria para celular 16M","barCode":"2345","seller":"Juan Barnes","price":32,"discount":false,"taxes":true,"image":"https://res.cloudinary.com/tecbases01bbb/image/upload/v1584593724/Screenshot_2020-03-18_International_Shopping_Shop_Computers_that_Ship_Internationally_8_sbfegj.png"},{"name":"Microfono","description":"Microfono 8mg","barCode":"2387","seller":"Juan Barnes","price":80,"discount":true,"taxes":true,"image":"https://res.cloudinary.com/tecbases01bbb/image/upload/v1584593723/Screenshot_2020-03-18_International_Shopping_Shop_Computers_that_Ship_Internationally_3_mwd9bm.png"},{"name":"Microfono","description":"Microfono 8mg","barCode":"1381","seller":"Juan Barnes","price":80,"discount":true,"taxes":true,"image":"https://res.cloudinary.com/tecbases01bbb/image/upload/v1584593723/Screenshot_2020-03-18_International_Shopping_Shop_Computers_that_Ship_Internationally_2_gwbmkf.png"}];
   constructor() { }
 
   @Input() set src(val:any){
