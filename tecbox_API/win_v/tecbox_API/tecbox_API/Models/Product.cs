a88513cebcb6871400be254d7867f04313168b92
Esteban Alvarado <estalvgs1999@gmail.com>
2020-03-21 20:11:42 -0600

    [Add] Product Model

--- tecbox_API/win_v/tecbox_API/tecbox_API/Models/Product.cs
+++ tecbox_API/win_v/tecbox_API/tecbox_API/Models/Product.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿/* ----------------------------------------------
+ * File: Product.cs
+ * Dev by: @estalvgs1999
+ * Project: TECbox API
+ * version: 2.1
+ * last edited by: @estalvgs1999 [21/03/2020]
+ *
+ * Description: Implementation of a tecbox 
+ * product.
+ * 
+ * TEC 2020 | CE3101 - Bases de Datos
+ * --------------------------------------------*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace tecbox_API.Models
 {
-    public class Class1
+    public class Product
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string BarCode { get; set; }
+        public string Seller { get; set; }
+        public int Price { get; set; }
+        public bool Discount { get; set; }
+        public string image { get; set; }
+
     }
 }
