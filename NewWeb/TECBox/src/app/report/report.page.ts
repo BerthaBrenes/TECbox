1f96b2dc8898e602ca5aaa43d1a3b9866cd7ad3f
Bertha Brenes <bertacaro1996@hotmail.com>
2020-05-17 16:21:09 -0600

    [ADD]merge with report

--- NewWeb/TECBox/src/app/report/report.page.ts
+++ NewWeb/TECBox/src/app/report/report.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
 /**
  * Component
  */
@@ -12,6 +14,22 @@ import { Component, OnInit } from '@angular/core';
  */
 export class ReportPage implements OnInit {
   /**
+   * Data of the component
+   */
+  data: any;
+  /**
+   * Report one for the best seller
+   */
+  reporte1: Array<any>;
+  /**
+   * Report for the ready to send
+   */
+  reporte2: Array<any>;
+  /**
+   * Report for the product in home
+   */
+  reporte3: Array<any>;
+  /**
    * First Function to call in the page
    */
   constructor() { }
@@ -20,5 +38,89 @@ export class ReportPage implements OnInit {
    */
   ngOnInit() {
   }
+  /**
+   * Generate the first report
+   */
+  generarReporte1(){
+    const pdf = new jsPDF();
+    pdf.setFontStyle('times');
+    pdf.setFontSize(30);
+    pdf.text(80, 20, 'TecBox');
+    pdf.text(35 , 30, 'Reporte de productos más vendidos');
+    const data = [];
+    const columns = ['Código de Barras', 'Nombre', 'Cantidad'];
 
+    // tslint:disable-next-line: prefer-for-of
+    for  (let x = 0; x < this.reporte1.length; x++){
+        console.log(this.reporte1[x]);
+        const temp = [];
+        temp.push(this.reporte1[x]['BarCode']);
+        temp.push(this.reporte1[x]['Name']);
+        temp.push(this.reporte1[x]['Qty']);
+        data.push(temp);
+    }
+    pdf.autoTable(columns, data, { margin: { top: 50 }, theme : 'grid'});
+    pdf.save('Productos más vendidos.pdf');
+    }
+    /**
+     * Generate second report
+     */
+    generarReporte2(){
+      const pdf = new jsPDF();
+      pdf.setFontStyle('times');
+      pdf.setFontSize(30);
+      pdf.text(80, 20, 'TecBox');
+      pdf.text(40 , 30, 'Productos listos para entregar');
+      const data = [];
+      const columns = ['TrackingId', 'Cliente', 'Descripción', 'IdRuta', 'Repartidor', 'IdRepartidor', 'Estado', 'Fecha de Entrega'];
+      // tslint:disable-next-line: prefer-for-of
+      for (let x = 0; x < this.reporte2.length; x++){
+          console.log(this.reporte2[x]);
+          const temp = [];
+          temp.push(this.reporte2[x]['TrackId']);
+          temp.push(this.reporte2[x]['Client']);
+          temp.push(this.reporte2[x]['Description']);
+          temp.push(this.reporte2[x]['RouteId']);
+          temp.push(this.reporte2[x]['DeliveryMan']);
+          temp.push(this.reporte2[x]['DmId']);
+          temp.push(this.reporte2[x]['Status']);
+          temp.push(this.reporte2[x]['DeliveryDate']);
+          data.push(temp);
+      }
+      pdf.autoTable(columns, data, { margin: { top: 100 }, theme : 'grid'});
+      pdf.save('Productos Entregados.pdf');
+    }
+    /**
+     * Generate third report
+     */
+    generarReporte3(){
+      const pdf = new jsPDF();
+      pdf.setFontStyle('times');
+      pdf.setFontSize(30);
+      pdf.text(80, 20, 'TecBox');
+      pdf.text(60 , 30, 'Productos entregados');
+      const data = [];
+      const columns = ['Repartidor', 'TrackingId', 'Cliente', 'Descripción', 'IdRuta', 'IdRepartidor', 'Estado', 'Fecha de Entrega'];
+      for (let x = 0; x <= this.reporte3.length; x++){
+        const temp2 = [];
+        console.log(this.reporte3[0]['1']);
+        temp2.push(this.reporte3[0]['' + x][0]['DeliveryMan']);
+        data.push(temp2);
+        // tslint:disable-next-line: prefer-for-of
+        for (let y = 0; y < this.reporte3[0]['' + x].length; y++){
+          const temp1 = [];
+          temp1.push('');
+          temp1.push(this.reporte3[0]['' + x][y]['TrackId']);
+          temp1.push(this.reporte3[0]['' + x][y]['Client']);
+          temp1.push(this.reporte3[0]['' + x][y]['Description']);
+          temp1.push(this.reporte3[0]['' + x][y]['RouteId']);
+          temp1.push(this.reporte3[0]['' + x][y]['DmId']);
+          temp1.push(this.reporte3[0]['' + x][y]['Status']);
+          temp1.push(this.reporte3[0]['' + x][y]['DeliveryDate']);
+          data.push(temp1);
+        }
+      }
+      pdf.autoTable(columns, data, { margin: { top: 50 }, theme : 'grid'});
+      pdf.save('Productos Entregados.pdf');
+    }
 }
